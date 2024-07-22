7cb8501bc85b4b9a9e8cc6258bdc550cdaf41506
Lucia Di Bello <lucia.dibello3@gmail.com>
2024-07-22 21:38:09 +0200

    ADD edit-delete file

--- spring-la-mia-pizzeria-crud/src/main/java/it/lamiapizzeriacrud/controller/PizzeriaController.java
+++ spring-la-mia-pizzeria-crud/src/main/java/it/lamiapizzeriacrud/controller/PizzeriaController.java
@@ -7,10 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.validation.BindingResult;
 import jakarta.validation.Valid;
 
@@ -67,5 +67,37 @@ public class PizzeriaController {
 	   return "redirect:/index";
 	}
 	
+	@GetMapping("/index/edit/{id}")
+	public String edit(@PathVariable("id") Integer id, Model model) {
+		
+		model.addAttribute("formPizza", repository.findById(id).get());
+		
+		return "edit";
+	}
+	
+	
+	@PostMapping("/index/edit/{id}")
+	public String updatePizza( @Valid @ModelAttribute("formPizza") Pizza formPizza, BindingResult bindingResult, Model model) {
+		
+		if(bindingResult.hasErrors()) {
+			return "edit";
+		}
+		
+		repository.save(formPizza);
+		
+		return "redirect:/index";
+	}
+	
+	
+	@PostMapping("/index/delete/{id}")
+	public String deletePizza(@PathVariable("id") Integer id) {
+		
+		repository.deleteById(id);
+		
+		return "redirect:/index";
+	}
+	
+
 }
+	
 	
